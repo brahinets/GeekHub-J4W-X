31bd6be81e1437ab128149175774ac9f9ac915d4
Yaroslav Brahinets <y.brahinets@gmail.com>
2020-10-24 19:50:01 +0300

    L01 Added students registry template

---
+++ Homework/src/org/geekhub/lesson01/StudentsRegistry.java
@@ -0,0 +1,21 @@
+package org.geekhub.lesson01;
+
+import java.util.Scanner;
+
+public class StudentsRegistry {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Total students count is expected as a first start argument but missing");
+            System.exit(1);
+        }
+        int totalStudentsCount = Integer.parseInt(args[0]); // Use me (and remove this comment)
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the student:");
+        String studentName = scanner.nextLine();
+        System.out.println(studentName);
+
+        System.exit(1); // Implement registry (and remove this line)
+    }
+}
